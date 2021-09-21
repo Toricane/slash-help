2d4f53733bd4d49198c2c307deba9e26313636a5
Toricane <73972068+Toricane@users.noreply.github.com>
2021-09-20 19:05:00 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+with open("README.md","r",encoding="utf-8") as fh:
+    long_description=fh.read()
+
+setup(
+    name="slash_help",
+    version="1.0.0",
+    description="Slash commands help for discord-interactions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Toricane/SlashHelp",
+    author="Toricane",
+    author_email="prjwl028@gmail.com",
+    license="MIT",
+    packages=["shash_help"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["discord-py-interactions", "discord.py"],
+)
