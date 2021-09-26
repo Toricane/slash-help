00fe944cf4d0a87ba7549ccb18a4d1aabb01fb75
Toricane <73972068+Toricane@users.noreply.github.com>
2021-09-26 16:58:39 -0700

    Update errors.py

--- slash_help/errors.py
+++ slash_help/errors.py
@@ -7,3 +7,15 @@ class CommandsNotFound(SlashHelpError):
         super().__init__(
             "No commands detected! If you are only using guild commands, try specifying the guild id!"
         )
+
+
+class NameNeeded(SlashHelpError):
+    def __init__(self):
+        super().__init__("No bot_name detected! Required if use_subcommand is True!")
+
+
+class IncorrectName(SlashHelpError):
+    def __init__(self):
+        super().__init__(
+            "Incorrect string passed into bot_name! \nMust match regex ^[\\w-]{1,32}$\nOnly a-z, 0-9, - and _"
+        )
