6b9a23e6533213228a9fbebc4ad852a2f6df7e08
Toricane <73972068+Toricane@users.noreply.github.com>
2021-09-28 19:19:26 -0700

    version up 1.4.1

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
-with open("README.md","r",encoding="utf-8") as fh:
-    long_description=fh.read()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="slash_help",
-    version="1.4.0",
+    version="1.4.1",
     description="Slash commands help for discord-interactions",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,5 +20,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["discord-py-interactions", "discord.py", "dinteractions-Paginator"],
+    install_requires=[
+        "discord-py-interactions",
+        "discord.py",
+        "dinteractions-Paginator",
+    ],
 )
