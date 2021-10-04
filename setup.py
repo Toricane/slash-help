ef3915a7889714415cb80c67ec02ce0ff05ba995
Toricane <73972068+Toricane@users.noreply.github.com>
2021-10-03 18:31:28 -0700

    version up 1.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="slash_help",
-    version="1.4.2",
+    version="1.5.0",
     description="discord-interactions slash command help",
     long_description=long_description,
     long_description_content_type="text/markdown",
